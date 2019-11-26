a6b93bbd5b2d77645d386ee18d4cc9a4601ed7ea
Natasha Ørregaard Klingenbrunn <natasha.klingenbrunn@gmail.com>
2019-11-26 16:43:28 +0100

    Finished model skeleton *Not Working*

--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,9 @@ from PIL import Image
 import torch
 import torch.nn as nn
 import os
+import models
+import torch.optim as optim
+from torch.autograd import Variable
 
 TRAINING_SIZE = 20
 NUM_EPOCHS = 100
@@ -25,14 +28,23 @@ def extract_feature_vectors(TRAINING_SIZE, data_dir, train_data_filename):
 
 def main():
 
-    data_dir = '../Datasets/training/'
+    data_dir = '../Datasets/training_debug/'
     train_data_filename = data_dir + 'images/'
     train_labels_filename = data_dir + 'groundtruth/'    
 
-    imgs = extract_feature_vectors(TRAINING_SIZE, data_dir, train_data_filename)
+    imgs =  extract_feature_vectors(TRAINING_SIZE, data_dir, train_data_filename)
     labels = extract_feature_vectors(TRAINING_SIZE, data_dir, train_labels_filename)
 
-    # Implement patches 
-    # Training for each patch
+    input = torch.Tensor(imgs[:][0])
+
+    DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu') 
+    loss = torch.nn.BCELoss()
+    model = models.UNET().to(DEVICE)
+    optimize = optim.Adam(model.parameters())
+
+    output = loss(model(input), labels)
+    output.backward()
+
+    print(output)
 
 main()
--- scripts/models.py
+++ scripts/models.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 
-from constants import * 
 
 # -------------------------------------------
 # U-NET
@@ -11,50 +9,40 @@ from constants import *
 # -------------------------------------------
 
 class UNET(nn.Module):
+
     def __init__(self, in_channels = 3, out_channels = 2):
-        super(UNET,self).__init():
-            # Contracting path
-            self.contract1 = self.doubleConv_block(in_channels, 64)
-            self.contract2 = self.doubleConv_block(64, 128)
-            self.contract3 = self.doubleConv_block(128, 256)
-            self.contract4 = self.doubleConv_block(256, 512)
-            
-            self.maxpool = torch.nn.MaxPool2d(kernel_size = 2)
-                 
-            # Expansive path
-            self.expand4 = self.expanding_block(512, 1024)
-            self.expand3 = self.expanding_block(1024, 512)
-            self.expand2 = self.expanding_block(512, 256)
-            self.expand1 = self.expanding_block(256, 128)
-            
-            # Final block
-            self.output = self.output_block(128, out_channels)
+
+        super(UNET,self).__init__()
+        # Contracting path
+        self.contract1 = self.doubleConv_block(in_channels, 64)
+        self.contract2 = self.doubleConv_block(64, 128)
+        self.contract3 = self.doubleConv_block(128, 256)
+        self.contract4 = self.doubleConv_block(256, 512)
+        
+        self.maxpool = torch.nn.MaxPool2d(kernel_size = 2)
+                
+        # Expansive path
+        self.expand4 = self.expanding_block(512, 1024)
+        self.expand3 = self.expanding_block(1024, 512)
+        self.expand2 = self.expanding_block(512, 256)
+        self.expand1 = self.expanding_block(256, 128)
+        
+        # Final block
+        self.output = self.output_block(128, out_channels)
             
     
     def doubleConv_block(self, in_channels, out_channels):
         """ (conv + ReLU + BN) * 2 times """
-            doubleConv_block = torch.nn.Sequential(torch.nn.Conv2d(in_channels, out_channels, kernel_size = 3),
-            torch.nn.ReLU(),  # apply activqtion function
-            # Position of Batch Normalization (BN) wrt nonlinearity unclear, but experiments are generally in favor of this solution, which is the current default(ReLU + BN) 
-            torch.nn.BatchNorm2d(out_channels),
-            torch.nn.Conv2d(out_channels, out_channels, kernel_size = 3),
-            torch.nn.ReLU(),
-            torch.nn.BatchNorm2d(out_channels)
-        )
+        doubleConv_block = torch.nn.Sequential(torch.nn.Conv2d(in_channels, out_channels, kernel_size = 3),
+        torch.nn.ReLU(),  # apply activqtion function
+        # Position of Batch Normalization (BN) wrt nonlinearity unclear, but experiments are generally in favor of this solution, which is the current default(ReLU + BN) 
+        torch.nn.BatchNorm2d(out_channels),
+        torch.nn.Conv2d(out_channels, out_channels, kernel_size = 3),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(out_channels))
+
         return doubleConv_block
-    
-    
-            
-    """
-    def contracting_block(self, in_channels, out_channels):
-         """ (conv + ReLU + BN) * 2 times + maxpool """
-        contracting_block = torch.nn.Sequential(
-            self.doubleConv_block(in_channels, out_channels),
-            torch.nn.MaxPool2d(kernel_size = 2)
-        )  
-        return contracting_block
-    """
-    
+
         
 
     def expanding_block(self, in_channels, tmp_channels):
@@ -85,28 +73,19 @@ class UNET(nn.Module):
         # Padding with reflection 
         # pad size found such that after doing all the convolutions n_out = n_in <=> n_padded = n_in + 184 (to be verified)
         pad = 92
-        layer0 = F.pad(x, (pad, pad, pad, pad), mode = 'reflect')
+        layer0 = F.pad(layer0, (pad, pad, pad, pad), mode = 'reflect')
+
         layer1_descending = self.contract1(layer0)
         layer2_descending = self.contract2(self.maxpool(layer1_descending))
         layer3_descending = self.contract3(self.maxpool(layer2_descending))
         layer4_descending = self.contract4(self.maxpool(layer3_descending))
         layer5 = self.maxpool(layer4_descending)
+
         layer4_ascending = self.expand4(self.concatenating_block(layer4_descending, layer5))
         layer3_ascending = self.expand3(self.concatenating_block(layer3_descending, layer4_ascending))
         layer2_ascending = self.expand2(self.concatenating_block(layer2_descending, layer3_ascending))
         layer1 = self.expand1(self.concatenating_block(layer1_descending, layer2_ascending))
+
         output = self.output(layer1)
         
         return output
-              
-  
-            
-            
-    def create_UNET():
-            network = UNET()
-            network.to(DEVICE)
-            criterion = 
-            optimize = optim.Adam(network.parameters())
-            return network, criterion, optimizer
-            
-        
