e5d51a74fb3d9b9580340a79bd82f59d986bd61e
dosarudaniel <dosarudaniel@gmail.com>
2019-11-28 12:02:30 +0100

    Added rotation

--- scripts/main.py
+++ scripts/main.py
@@ -23,11 +23,13 @@ def getPatches(image, patch_h, patch_w):
             patches.append(patch)
     return patches
 
-def extract_feature_vectors(TRAINING_SIZE, data_dir, train_data_filename):
+def extract_feature_vectors(TRAINING_SIZE, data_dir, path, rotate = True, save = False):
 
+    train_data_filename = data_dir + path
     to_tensor = transforms.ToTensor() #ToTensor transforms the image to a tensor with range [0,1]
     num_images = TRAINING_SIZE
     imgs = []
+    r_imgs = []
 
     for i in range(1, num_images+1):
 
@@ -36,23 +38,40 @@ def extract_feature_vectors(TRAINING_SIZE, data_dir, train_data_filename):
         print(image_filename)
         if os.path.isfile(image_filename):
             img = Image.open(image_filename)
-
             t_img = to_tensor(img).unsqueeze(0) # 1 x 3 [rgb] x 400 x 400
-
             imgs.append(t_img)
-    return imgs # length TRAINING_SIZE
+
+            if (rotate == True):
+                width, height = img.size
+                # Rotate images
+
+                for degree in range(360):
+                    rotated_img = img.rotate(degree)
+                    # Crop to remove black parts
+                    left     = width / 4 * 0.58
+                    top      = height / 4 * 0.58
+                    right    = width - left
+                    bottom   = height - top
+                    rotated_img = rotated_img.crop((left, top, right, bottom))
+                    if (save == True):
+                        # optional, save new image on disk, to see the effect
+                        rotated_img.save("../Rotations/"+path + imageid + "_"+str(degree)+".png")
+                    rt_img = to_tensor(rotated_img).unsqueeze(0) # 1 x 3 [rgb] x 284 x 284
+                    r_imgs.append(rt_img)
+
+    return imgs, r_imgs
 
 def main():
 
     data_dir = '../Datasets/training/'
-    train_data_filename = data_dir + 'images/'
-    train_labels_filename = data_dir + 'groundtruth/'
+    train_data_filename = 'images/'
+    train_labels_filename = 'groundtruth/'
     patch_h = 32
     patch_w = 32
 
 
-    imgs =  extract_feature_vectors(TRAINING_SIZE, data_dir, train_data_filename)
-    labels = extract_feature_vectors(TRAINING_SIZE, data_dir, train_labels_filename)
+    imgs, r_imgs =  extract_feature_vectors(TRAINING_SIZE, data_dir, train_data_filename, True)
+    labels, r_labels = extract_feature_vectors(TRAINING_SIZE, data_dir, train_labels_filename, True)
 
     input = torch.Tensor(imgs[:][0])
 
